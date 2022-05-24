6b48b2228935f37e9012d7a44c926e6abfa2794e
Ed Leijnse <ed@leijnse.info>
2022-05-24 21:25:58 +0200

    insert and delete added

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -32,53 +32,134 @@ class MyView : View() {
     // var hyperLinksList = listOf("").toMutableList().asObservable()
     val hyperLinksList = FXCollections.observableArrayList<String>(
     )
+    val ID = SimpleStringProperty()
+    val group = SimpleStringProperty()
+    val category = SimpleStringProperty()
+    val webdescription = SimpleStringProperty()
+    val website = SimpleStringProperty()
 
     var hyperLinksListSimple = listOf("")
     var selectedUrl = "nothing selected"
 
     override val root = form {
+        // https://edvin.gitbooks.io/tornadofx-guide/content/part1/7_Layouts_and_Menus.html
         input.value = ""
-        setPrefSize(1000.0, 600.0)
-        fieldset {
-            field("Input") {
-                textfield(input)
-            }
+        ID.value = ""
+        group.value = ""
+        category.value = ""
+        webdescription.value = ""
+        website.value = ""
+        tabpane{
+            tab("search"){
+                input.value = ""
+                setPrefSize(1000.0, 600.0)
+                fieldset ("search for hyperlinks"){
+                    field("Input") {
+                        textfield(input)
+                    }
 
-            button("search") {
-                action {
-                    hyperLinksList.clear()
-                    hyperLinksListSimple = controller.readHyperlinks(input.value)
-                    for (hyperlinkItem in hyperLinksListSimple) {
-                        hyperLinksList.add(hyperlinkItem)
+                    button("search") {
+                        action {
+                            hyperLinksList.clear()
+                            hyperLinksListSimple = controller.readHyperlinks(input.value)
+                            for (hyperlinkItem in hyperLinksListSimple) {
+                                hyperLinksList.add(hyperlinkItem)
+                            }
+                            input.value = ""
+                        }
+                        style {
+                            textFill = Color.RED
+                        }
                     }
-                    input.value = ""
-                }
-                style {
-                    textFill = Color.RED
-                }
-            }
-            listview(hyperLinksList) {
-                selectionModel.selectionMode = SelectionMode.SINGLE
-                //TableColumnHeader columnHeader =
-                // onUserSelect(1) { "print you selected" }
+                    listview(hyperLinksList) {
+                        selectionModel.selectionMode = SelectionMode.SINGLE
+                        //TableColumnHeader columnHeader =
+                        // onUserSelect(1) { "print you selected" }
 
-                onDoubleClick {
-                    selectedUrl = MyController().extractUrl(selectedItem.toString())
-                    println("double click: $selectedUrl")
-                    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                        Desktop.getDesktop().browse(URI(selectedUrl));
+                        onDoubleClick {
+                            selectedUrl = MyController().extractUrl(selectedItem.toString())
+                            println("double click: $selectedUrl")
+                            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                                Desktop.getDesktop().browse(URI(selectedUrl));
+                            }
+                        }
+                    }
+                    hyperlink("double click on list to go to hyperlink") {
+                        action {
+                            println("hyperlink clicked: $selectedUrl")
+                            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                                Desktop.getDesktop().browse(URI(selectedUrl));
+                            }
+                        }
                     }
                 }
-            }
-            hyperlink("double click on list to go to hyperlink") {
-                action {
-                    println("hyperlink clicked: $selectedUrl")
-                    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                        Desktop.getDesktop().browse(URI(selectedUrl));
+                tab("insert"){
+                    setPrefSize(1000.0, 600.0)
+                    fieldset ("insert a hyperlink"){
+                        hbox{
+                            label("ID                       ")
+                            field("ID"){
+                                textfield(ID)
+                            }
+                        }
+                        hbox{
+                            label("group                 ")
+                            field("group"){
+                                textfield(group)
+                            }
+                        }
+                        hbox{
+                            label("category             ")
+                            field("category"){
+                                textfield(category)
+                            }
+                        }
+                        hbox{
+                            label("webdescription  ")
+                            field("webdescription"){
+                                textfield(webdescription)
+                            }
+                        }
+                        hbox{
+                            label("website              ")
+                            field("website"){
+                                textfield(website)
+                            }
+                        }
+                        button("insert") {
+                            action {
+                                controller.insertHyperLink(ID.value, group.value, category.value,webdescription.value,website.value)
+                            }
+                            style {
+                                textFill = Color.RED
+                            }
+                        }
                     }
+
+                }
+                tab("delete"){
+                    setPrefSize(1000.0, 600.0)
+                    fieldset ("delete a hyperlink"){
+                        hbox{
+                            label("ID                       ")
+                            field("ID"){
+                                textfield(ID)
+                            }
+                            button("delete") {
+                                action {
+                                    controller.deleteHyperLink(ID.value)
+                                }
+                                style {
+                                    textFill = Color.RED
+                                }
+                            }
+                        }
+                    }
+
                 }
             }
         }
+
     }
 }
 
@@ -96,6 +177,38 @@ class MyController : Controller() {
         }
 
         return outputUrl
+    }
+    fun insertHyperLink(ID: String, group: String, category: String, webdescription: String, website: String): String {
+        var resultString: String = ""
+        println("ID: $ID group:$group category:$category webdescription:$webdescription website:$website")
+        val encodedID = URLEncoder.encode(ID, "utf-8")
+        val encodedGroup = URLEncoder.encode(group, "utf-8")
+        val encodedCategory = URLEncoder.encode(category, "utf-8")
+        val encodedWebdescription = URLEncoder.encode(webdescription, "utf-8")
+        val encodedWebsite = URLEncoder.encode(website, "utf-8")
+        val url = URL(
+            "https://leijnse.info/hyperlinks/rest/Restcontroller.php/?command=insertmysql&ID=$encodedID&group=$encodedGroup&category=$encodedCategory&webdescription=$encodedWebdescription&website=$encodedWebsite"
+
+        )
+        val jsonData = url.readText()
+        println("output: $jsonData")
+
+        resultString= "OK"
+        return resultString
+    }
+    fun deleteHyperLink(ID: String): String {
+        var resultString: String = ""
+        println("ID: $ID ")
+        val encodedID = URLEncoder.encode(ID, "utf-8")
+        val url = URL(
+            "https://leijnse.info/hyperlinks/rest/Restcontroller.php/?command=deletemysql&ID=$encodedID"
+
+        )
+        val jsonData = url.readText()
+        println("output: $jsonData")
+
+        resultString= "OK"
+        return resultString
     }
     fun readHyperlinks(searchString: String): List<String> {
         println("hyperlinks restcall with $searchString ")
