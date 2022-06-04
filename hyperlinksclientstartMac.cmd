6da13f5b4afb69b146541dacc844e3bdeb9c4089
Ed Leijnse <ed@leijnse.info>
2022-06-04 17:25:52 +0200

    hyperlinksclientstartMac.cmd added to start hyperlink client on Mac

---
+++ hyperlinksclientstartMac.cmd
@@ -0,0 +1,2 @@
+cd /Users/edleijnse/hyperlinks
+java --module-path /Library/Java/JavaVirtualMachines/javafx-sdk-18.0.1/lib/  --add-modules javafx.controls,javafx.fxml  -jar /Users/edleijnse/hyperlinks/HyperlinksClient_main_jar/HyperlinksClient.main.jar
